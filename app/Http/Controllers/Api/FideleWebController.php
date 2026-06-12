df565a6771288b05114ff5044a2795e3c35399bf
ndsuffragessoanierana-hub <ndsuffrages.soanierana@gmail.com>
2026-06-12 14:31:11 +0300

    rectif Fidele 8

--- app/Http/Controllers/Api/FideleWebController.php
+++ app/Http/Controllers/Api/FideleWebController.php
@@ -97,7 +97,7 @@ class fideleWebController extends Controller
 	// Export PDF
 	public function exportPdf(Request $request)
 	{
-		ini_set('memory_limit', '512M');
+		ini_set('memory_limit', '1024M');
 		set_time_limit(120);
 
 		// 🔥 IMPORTANT : chunk au lieu de get massif
