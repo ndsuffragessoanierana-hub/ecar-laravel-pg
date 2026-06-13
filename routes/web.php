c1b536439e30790c070a549ca6910c103affaf23
ndsuffragessoanierana-hub <ndsuffrages.soanierana@gmail.com>
2026-06-13 12:33:58 +0300

    rectif Fidele 11 Excel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BienController;
 use App\Http\Controllers\Api\FideleWebController;
+use App\Http\Controllers\JournalController;
 
 // Accueil
 Route::get('/', [HomeController::class, 'index'])->name('home');
